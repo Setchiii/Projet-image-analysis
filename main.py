d7dd9942800c47369635952790a79d4bec111eca
Max Foccroulle <foccroullemax@gmail.com>
2024-02-06 14:15:21 +0100

    add a final message in the main.py file

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import toolbox
 men1_img = Image.open(".\\images\\men1.jpg")
 men1_img_threshold = toolbox.threshold(men1_img, 150)
 men1_img_threshold.save("men1_img_threshold.jpg", "JPEG")
+print("Thresholding done")
